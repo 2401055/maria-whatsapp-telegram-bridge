dff5a00cafa800af591d4b809b2144e72c02a121
2401055 <2.34778998e+08+2401055@users.noreply.github.com>
2026-07-14 23:18:20 +0000

    Improve process management and logging in index.js

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
+const { spawn } = require('child_process');
+const path = require('path');
 
-const {
-   spawn
-} = require('child_process')
-const path = require('path')
+console.log('--- Starting Maria-MD Bridge ---');
+console.log('Checking environment...');
 
 function start() {
-   let args = [path.join(__dirname, 'plugins.js'), ...process.argv.slice(2)]
-   console.log([process.argv[0], ...args].join('\n'))
-   let p = spawn(process.argv[0], args, {
-         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-      })
-      .on('message', data => {
-         if (data == 'reset') {
-            console.log('Restarting Bot...')
-            p.kill()
-            start()
-            delete p
-         }
-      })
-      .on('exit', code => {
-         console.error('Exited with code:', code)
-         if (code == '.' || code == 1 || code == 0) start()
-      })
+    console.log('Spawning plugins.js process...');
+    let args = [path.join(__dirname, 'plugins.js'), ...process.argv.slice(2)];
+    
+    let p = spawn(process.argv[0], args, {
+        stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+    });
+
+    p.on('message', data => {
+        if (data == 'reset') {
+            console.log('Restarting Bot...');
+            p.kill();
+            start();
+        }
+    });
+
+    p.on('exit', code => {
+        console.error('Process exited with code:', code);
+        // Restart on any exit to keep it alive
+        setTimeout(start, 5000);
+    });
+
+    p.on('error', err => {
+        console.error('Failed to start process:', err);
+    });
 }
-start()
+
+start();
